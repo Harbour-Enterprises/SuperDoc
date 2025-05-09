6830f2947f2559b5a3b04b01bfecab85cb4dc5dc
Nick Bernal <nick@harbourshare.com>
2025-05-08 20:16:41 -0700

    Updated all tests except importExportLists for Lists v2

--- packages/super-editor/src/tests/export/lists/miscOrderedListExport.test.js
+++ packages/super-editor/src/tests/export/lists/miscOrderedListExport.test.js
@@ -43,7 +43,7 @@ describe('[orderedlist_interrupted1.docx] interrupted ordered list tests', async
     expect(firstListPprList.length).toBe(1);
 
     const firstListPpr = firstListPprList[0];
-    expect(firstListPpr.elements.length).toBe(1);
+    expect(firstListPpr.elements.length).toBe(3);
 
     // Ensure that we only have 1 pPr tag
     const firstListNumPrList = firstListPpr.elements.filter((n) => n.name === 'w:numPr');
