aedfe6b909ce7ba7c421428a370d5f97eaa81a46
Caio Pizzol <caio@harbourshare.com>
2025-09-05 13:02:08 -0300

    docs: add JSDoc documentation for DropCursor extension options
    - Introduced detailed JSDoc comments for DropCursor options, specifying types and default values.
    - Enhanced organization of documentation for improved clarity and usability.

--- packages/super-editor/src/extensions/dropcursor/dropcursor.js
+++ packages/super-editor/src/extensions/dropcursor/dropcursor.js
@@ -1,10 +1,23 @@
+// @ts-check
 import { Extension } from '@core/index.js';
 import { dropCursor } from 'prosemirror-dropcursor';
 
+/**
+ * @module DropCursor
+ * @sidebarTitle Drop Cursor
+ * @snippetPath /snippets/extensions/dropcursor.mdx
+ */
 export const DropCursor = Extension.create({
   name: 'dropCursor',
 
   addOptions() {
+    /**
+     * @typedef {Object} DropCursorOptions
+     * @category Options
+     * @property {string} [color='currentColor'] - CSS color for the drop cursor indicator
+     * @property {number} [width=2] - Width of the drop cursor line in pixels
+     * @property {string} [class] - Optional CSS class to apply to the drop cursor element
+     */
     return {
       color: 'currentColor',
       width: 2,
