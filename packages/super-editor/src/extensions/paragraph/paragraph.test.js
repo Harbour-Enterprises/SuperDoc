02966f4555e0c1c949ed0386745d7d041bc5c1e5
Luccas Correa <luccas@harbourshare.com>
2025-09-09 13:58:23 -0300

    feat: remove the heading node from superdoc (#901)
    * feat: parse HTML headings as styled paragraph nodes
    
    * docs: add jsdoc for linked styles commands and helpers
    
    * feat: add command for toggling linked style
    
    * refactor: turn heading node into extension
    
    * refactor: adjust heading commands to work with styled paragraphs
    
    * fix: bug while retrieving style by id
    
    * test: add tests for LinkedStyles extension
    
    * test: add heading extension tests
    
    * test: simulate inserting heading using html string
    
    * test: simulate adding plain text string as paragraph
    
    ---------
    
    Co-authored-by: Luccas Correa <luccas@superdoc.dev>

---
+++ packages/super-editor/src/extensions/paragraph/paragraph.test.js
@@ -0,0 +1,113 @@
+import { beforeEach, describe, expect, it, vi } from 'vitest';
+import { initTestEditor, loadTestDataForEditorTests } from '../../tests/helpers/helpers.js';
+import { exportSchemaToJson } from '../../core/super-converter/exporter.js';
+
+describe('Paragraph Node', () => {
+  let docx, media, mediaFiles, fonts, editor, tr;
+  beforeAll(async () => {
+    ({ docx, media, mediaFiles, fonts } = await loadTestDataForEditorTests('blank-doc.docx'));
+  });
+
+  beforeEach(() => {
+    ({ editor } = initTestEditor({ content: docx, media, mediaFiles, fonts }));
+    tr = editor.state.tr;
+    vi.clearAllMocks();
+  });
+
+  it('inserting html with <h1> tag adds paragraph styled as heading', async () => {
+    editor.commands.insertContent('<h1>Test Heading</h1>');
+    expect(editor.state.doc.content.content[0].type.name).toBe('paragraph');
+    expect(editor.state.doc.content.content[0].attrs.styleId).toBe('Heading1');
+
+    const result = await editor.exportDocx({
+      exportJsonOnly: true,
+    });
+
+    const body = result.elements[0];
+
+    expect(body.elements).toHaveLength(2);
+    expect(body.elements.map((el) => el.name)).toEqual(['w:p', 'w:sectPr']);
+    const paragraph = body.elements[0];
+    expect(paragraph.name).toBe('w:p');
+    expect(paragraph.elements).toEqual([
+      {
+        name: 'w:pPr',
+        elements: [
+          {
+            name: 'w:pStyle',
+            attributes: {
+              'w:val': 'Heading1',
+            },
+          },
+          {
+            name: 'w:spacing',
+            attributes: {
+              'w:before': 0,
+              'w:after': 0,
+              'w:lineRule': 'auto',
+            },
+          },
+        ],
+      },
+      {
+        name: 'w:r',
+        elements: [
+          {
+            name: 'w:t',
+            elements: [
+              {
+                text: 'Test Heading',
+                type: 'text',
+              },
+            ],
+            attributes: null,
+          },
+        ],
+      },
+    ]);
+  });
+
+  it('inserting plain text creates a simple paragraph', async () => {
+    editor.commands.insertContent('This is a test paragraph.');
+    expect(editor.state.doc.content.content[0].type.name).toBe('paragraph');
+    expect(editor.state.doc.content.content[0].attrs.styleId).toBe(null);
+    const result = await editor.exportDocx({
+      exportJsonOnly: true,
+    });
+
+    const body = result.elements[0];
+
+    expect(body.elements).toHaveLength(2);
+    expect(body.elements.map((el) => el.name)).toEqual(['w:p', 'w:sectPr']);
+    const paragraph = body.elements[0];
+    expect(paragraph.name).toBe('w:p');
+    expect(paragraph.elements).toEqual([
+      {
+        name: 'w:pPr',
+        elements: [
+          {
+            name: 'w:spacing',
+            attributes: {
+              'w:lineRule': 'auto',
+            },
+          },
+        ],
+      },
+      {
+        name: 'w:r',
+        elements: [
+          {
+            name: 'w:t',
+            elements: [
+              {
+                text: 'This is a test paragraph.',
+                type: 'text',
+              },
+            ],
+            attributes: null,
+          },
+        ],
+      },
+    ]);
+  });
+});
