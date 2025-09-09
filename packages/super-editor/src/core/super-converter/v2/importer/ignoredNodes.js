e38ce1f79043a6327b617007284bbdf0a32572ea
Nick Bernal <nick@harbourshare.com>
2025-09-08 22:14:27 -0700

    chore: add ignored node

--- packages/super-editor/src/core/super-converter/v2/importer/ignoredNodes.js
+++ packages/super-editor/src/core/super-converter/v2/importer/ignoredNodes.js
@@ -1,6 +1,6 @@
 // Nodes listed here will be completely ignored during import.
 // Add any XML node names that should be skipped.
-export const IGNORED_NODE_NAMES = ['w:proofErr'];
+export const IGNORED_NODE_NAMES = ['w:proofErr', 'w:lastRenderedPageBreak'];
 
 /**
  * Recursively removes all nodes whose names appear in `IGNORED_NODE_NAMES`.
@@ -11,6 +11,4 @@ export const IGNORED_NODE_NAMES = ['w:proofErr'];
 export const pruneIgnoredNodes = (nodes = []) =>
   nodes
     .filter((node) => !IGNORED_NODE_NAMES.includes(node.name))
-    .map((node) =>
-      node.elements ? { ...node, elements: pruneIgnoredNodes(node.elements) } : node,
-    );
+    .map((node) => (node.elements ? { ...node, elements: pruneIgnoredNodes(node.elements) } : node));
