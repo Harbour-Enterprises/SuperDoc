aa8150a2b98608d4a722595729acbd440d9806f9
Luccas Correa <luccas@superdoc.dev>
2025-09-11 14:26:15 -0300

    refactor: bind encodeFn and decodeFn calls in node-translator
    The goal is to allow the functions to call other methods in the node
    translator from their implementation.

--- packages/super-editor/src/core/super-converter/v3/node-translator/node-translator.js
+++ packages/super-editor/src/core/super-converter/v3/node-translator/node-translator.js
@@ -170,7 +170,7 @@ export class NodeTranslator {
    */
   decode(params) {
     const decodedAttrs = this.decodeAttributes(params);
-    return this.decodeFn ? this.decodeFn(params, decodedAttrs) : undefined;
+    return this.decodeFn ? this.decodeFn.call(this, params, decodedAttrs) : undefined;
   }
 
   /**
@@ -180,7 +180,7 @@ export class NodeTranslator {
    */
   encode(params) {
     const encodedAttrs = this.encodeAttributes(params);
-    return this.encodeFn ? this.encodeFn(params, encodedAttrs) : undefined;
+    return this.encodeFn ? this.encodeFn.call(this, params, encodedAttrs) : undefined;
   }
 
   /**
