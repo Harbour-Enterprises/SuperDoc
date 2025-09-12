7b4300204f5ac2cb32b7a6de88c86d1ed940f587
Luccas Correa <luccas@superdoc.dev>
2025-09-12 17:39:03 -0300

    feat: add v3 translator for w:tr element

--- packages/super-editor/src/core/super-converter/v3/handlers/w/tr/tr-translator.js
+++ packages/super-editor/src/core/super-converter/v3/handlers/w/tr/tr-translator.js
@@ -1,5 +1,11 @@
 // @ts-check
 import { NodeTranslator } from '@translator';
+import { twipsToPixels, pixelsToTwips } from '@core/super-converter/helpers.js';
+import { createAttributeHandler } from '@converter/v3/handlers/utils.js';
+
+import { translateChildNodes } from '@core/super-converter/v2/exporter/helpers/index.js';
+import { translator as tcTranslator } from '../tc';
+import { translator as trPrTranslator } from '../trPr';
 
 /** @type {import('@translator').XmlNodeName} */
 const XML_NODE_NAME = 'w:tr';
@@ -13,4 +19,129 @@ const SD_NODE_NAME = 'tableRow';
  * @type {import('@translator').AttrConfig[]}
  * @see {@link https://ecma-international.org/publications-and-standards/standards/ecma-376/} "Fundamentals And Markup Language Reference", page 472
  */
-const validXmlAttributes = ['w:rsidDel', 'w:rsidR', 'w:rsidRPr', 'w:rsidTr'].map(_createAttributeHandler);
+const validXmlAttributes = ['w:rsidDel', 'w:rsidR', 'w:rsidRPr', 'w:rsidTr', 'w14:paraId', 'w14:textId'].map(
+  (xmlName) => createAttributeHandler(xmlName),
+);
+
+/**
+ * Encode a w:tr element as a SuperDoc 'tableRow' node.
+ * @param {import('@translator').SCEncoderConfig} [params]
+ * @param {import('@translator').EncodedAttributes} [encodedAttrs] - The already encoded attributes
+ * @returns {import('@translator').SCEncoderResult}
+ */
+const encode = (params, encodedAttrs) => {
+  const { row, table } = params.extraParams;
+
+  let tableRowProperties = {};
+  const tPr = row.elements.find((el) => el.name === 'w:trPr');
+  if (tPr) {
+    ({ attributes: tableRowProperties } = trPrTranslator.encode({
+      ...params,
+      nodes: [tPr],
+    }));
+  }
+  encodedAttrs['tableRowProperties'] = Object.freeze(tableRowProperties);
+
+  // Move some properties up a level for easier access
+  encodedAttrs['rowHeight'] = twipsToPixels(tableRowProperties['rowHeight']?.value);
+  encodedAttrs['cantSplit'] = tableRowProperties['cantSplit'];
+
+  // Handling cells
+  const gridColumnWidths = _getGridColumnWidths(table);
+  const cellNodes = row.elements.filter((el) => el.name === 'w:tc');
+  let currentColumnIndex = 0;
+  const content =
+    cellNodes?.map((n) => {
+      let columnWidth = gridColumnWidths?.[currentColumnIndex] || null;
+
+      const result = tcTranslator.encode({
+        ...params,
+        extraParams: {
+          ...params.extraParams,
+          node: n,
+          columnIndex: currentColumnIndex,
+          columnWidth,
+        },
+      });
+
+      const tcPr = n.elements?.find((el) => el.name === 'w:tcPr');
+      const colspanTag = tcPr?.elements?.find((el) => el.name === 'w:gridSpan');
+      const colspan = parseInt(colspanTag?.attributes['w:val'] || 1, 10);
+      currentColumnIndex += colspan;
+
+      return result;
+    }) || [];
+
+  const newNode = {
+    type: 'tableRow',
+    content,
+    attrs: encodedAttrs,
+  };
+  return newNode;
+};
+
+/**
+ * Get the column widths from the table grid (w:tblGrid).
+ * @param {Object} [tableNode] The w:tbl node
+ * @returns {number[]} Array of column widths in pixels
+ */
+const _getGridColumnWidths = (tableNode) => {
+  const tblGrid = tableNode.elements.find((el) => el.name === 'w:tblGrid');
+  if (!tblGrid) return [];
+  const columnWidths =
+    tblGrid?.elements?.flatMap((el) => {
+      if (el.name !== 'w:gridCol') return [];
+      return twipsToPixels(el.attributes['w:w']);
+    }) || [];
+  return columnWidths;
+};
+
+/**
+ * Decode the tableRow node back into OOXML <w:tr>.
+ * @param {import('@translator').SCDecoderConfig} params
+ * @param {import('@translator').DecodedAttributes} [decodedAttrs] - The already decoded attributes
+ * @returns {import('@translator').SCDecoderResult}
+ */
+const decode = (params, decodedAttrs) => {
+  const { node } = params;
+  const elements = translateChildNodes(params);
+  if (node.attrs?.tableRowProperties) {
+    const tableRowProperties = { ...node.attrs.tableRowProperties };
+    // Update rowHeight and cantSplit in tableRowProperties if they exist
+    if (node.attrs.rowHeight != null) {
+      const rowHeightPixels = twipsToPixels(node.attrs.tableRowProperties['rowHeight']?.value);
+      if (rowHeightPixels !== node.attrs.rowHeight) {
+        // If the value has changed, update it
+        tableRowProperties['rowHeight'] = { value: String(pixelsToTwips(node.attrs['rowHeight'])) };
+      }
+    }
+    tableRowProperties['cantSplit'] = node.attrs['cantSplit'];
+    const trPr = trPrTranslator.decode({
+      ...params,
+      node: { ...node, attrs: { ...node.attrs, tableRowProperties } },
+    });
+    if (trPr) elements.unshift(trPr);
+  }
+
+  return {
+    name: 'w:tr',
+    attributes: decodedAttrs || {},
+    elements,
+  };
+};
+
+/** @type {import('@translator').NodeTranslatorConfig} */
+export const config = {
+  xmlName: XML_NODE_NAME,
+  sdNodeOrKeyName: SD_NODE_NAME,
+  type: NodeTranslator.translatorTypes.NODE,
+  encode,
+  decode,
+  attributes: validXmlAttributes,
+};
+
+/**
+ * The NodeTranslator instance for the passthrough element.
+ * @type {import('@translator').NodeTranslator}
+ */
+export const translator = NodeTranslator.from(config);
