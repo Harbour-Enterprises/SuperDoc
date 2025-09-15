bea83db1e63aa61c46c2f313835ee2c6e252eca0
Luccas Correa <luccas@superdoc.dev>
2025-09-15 19:01:00 -0300

    feat: add translator for w:cnfStyle

---
+++ packages/super-editor/src/core/super-converter/v3/handlers/w/cnfStyle/cnfStyle-translator.js
@@ -0,0 +1,43 @@
+import { NodeTranslator } from '@translator';
+import { createAttributeHandler } from '@converter/v3/handlers/utils.js';
+
+/**
+ * The NodeTranslator instance for the cnfStyle element.
+ * @type {import('@translator').NodeTranslator}
+ * @see {@link https://ecma-international.org/publications-and-standards/standards/ecma-376/} "Fundamentals And Markup Language Reference", page 379
+ *
+ */
+export const translator = NodeTranslator.from({
+  xmlName: 'w:cnfStyle',
+  sdNodeOrKeyName: 'cnfStyle',
+  attributes: [
+    'w:evenHBand',
+    'w:evenVBand',
+    'w:firstColumn',
+    'w:firstRow',
+    'w:firstRowFirstColumn',
+    'w:firstRowLastColumn',
+    'w:lastColumn',
+    'w:lastRow',
+    'w:lastRowFirstColumn',
+    'w:lastRowLastColumn',
+    'w:oddHBand',
+    'w:oddVBand',
+    'w:val',
+  ].map((attr) => createAttributeHandler(attr)),
+  encode: (_, encodedAttrs) => {
+    // Convert '1'/'0' and 'true'/'false' to boolean
+    Object.keys(encodedAttrs).forEach((key) => {
+      encodedAttrs[key] = ['1', 'true'].includes(encodedAttrs[key]);
+    });
+    return Object.keys(encodedAttrs).length > 0 ? encodedAttrs : undefined;
+  },
+  decode: ({ node }) => {
+    if (!node.attrs?.cnfStyle) return;
+    const cnfStyleAttrs = {};
+    Object.entries(node.attrs.cnfStyle).forEach(([key, value]) => {
+      cnfStyleAttrs[`w:${key}`] = value ? '1' : '0';
+    });
+    return Object.keys(cnfStyleAttrs).length > 0 ? cnfStyleAttrs : undefined;
+  },
+});
