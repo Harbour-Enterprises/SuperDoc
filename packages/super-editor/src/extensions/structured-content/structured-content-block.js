0c482690964964ea3a83680ec899de29070230b7
Artem Nistuley <artem@harbourshare.com>
2025-09-18 15:44:22 +0300

    feat: structured content node views

--- packages/super-editor/src/extensions/structured-content/structured-content-block.js
+++ packages/super-editor/src/extensions/structured-content/structured-content-block.js
@@ -1,16 +1,26 @@
-import { Node, Attribute } from '@core/index.js';
+import { Node, Attribute } from '@core/index';
+import { StructuredContentBlockView } from './StructuredContentBlockView';
+
+export const structuredContentClass = 'sd-structured-content-block';
+export const structuredContentInnerClass = 'sd-structured-content-block__content';
 
 export const StructuredContentBlock = Node.create({
   name: 'structuredContentBlock',
 
-  group: 'block',
+  group: 'block structuredContent',
 
   content: 'block*',
 
+  isolating: true,
+
+  atom: false, // false - has editable content.
+
+  draggable: true,
+
   addOptions() {
     return {
-      structuredContentClass: 'sd-structured-content-block-tag',
       htmlAttributes: {
+        class: structuredContentClass,
         'aria-label': 'Structured content block node',
       },
     };
@@ -18,6 +28,15 @@ export const StructuredContentBlock = Node.create({
 
   addAttributes() {
     return {
+      id: {
+        default: null,
+        parseDOM: (elem) => elem.getAttribute('data-id'),
+        renderDOM: (attrs) => {
+          if (!attrs.id) return {};
+          return { 'data-id': attrs.id };
+        },
+      },
+
       sdtPr: {
         rendered: false,
       },
@@ -25,16 +44,22 @@ export const StructuredContentBlock = Node.create({
   },
 
   parseDOM() {
-    return [{ tag: `div.${this.options.structuredContentClass}` }];
+    return [{ tag: 'div[data-structured-content-block]' }];
   },
 
   renderDOM({ htmlAttributes }) {
     return [
       'div',
       Attribute.mergeAttributes(this.options.htmlAttributes, htmlAttributes, {
-        class: this.options.structuredContentClass,
+        'data-structured-content-block': '',
       }),
       0,
     ];
+  },
+
+  addNodeView() {
+    return (props) => {
+      return new StructuredContentBlockView({ ...props });
+    };
   },
 });
