b7162d3194b758bb8f45012a2aa920f77904810d
Caio Pizzol <caio@harbourshare.com>
2025-10-08 08:27:33 -0300

    refactor: remove getStructuredContentTagsByAlias and related command

--- packages/super-editor/src/extensions/structured-content/structuredContentHelpers/index.js
+++ packages/super-editor/src/extensions/structured-content/structuredContentHelpers/index.js
@@ -2,4 +2,3 @@ export * from './getStructuredContentTags';
 export * from './getStructuredContentInlineTags';
 export * from './getStructuredContentBlockTags';
 export * from './getStructuredContentTagsById';
-export * from './getStructuredContentTagsByAlias';
