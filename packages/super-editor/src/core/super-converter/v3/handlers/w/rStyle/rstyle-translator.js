2daabf8860405bbbe07ab0efdac9a192a308d9d0
Luccas Correa <luccas@superdoc.dev>
2025-10-13 13:35:55 -0300

    refactor: update w:rStyle translator to use utility functions

--- packages/super-editor/src/core/super-converter/v3/handlers/w/rStyle/rstyle-translator.js
+++ packages/super-editor/src/core/super-converter/v3/handlers/w/rStyle/rstyle-translator.js
@@ -1,43 +1,9 @@
-// @ts-check
+import { createSingleAttrPropertyHandler } from '@converter/v3/handlers/utils.js';
 import { NodeTranslator } from '@translator';
-import validXmlAttributes from './attributes/index.js';
-
-/** @type {import('@translator').XmlNodeName} */
-const XML_NODE_NAME = 'w:rStyle';
-
-/** @type {import('@translator').SuperDocNodeOrKeyName} */
-const SD_ATTR_KEY = 'styleId';
-
-/**
- * Encode the w:rStyle element.
- * Pass through attributes (typically { 'w:val': styleId }).
- * @param {import('@translator').SCEncoderConfig} params
- * @returns {import('@translator').SCEncoderResult}
- */
-const encode = (params, encodedAttrs = {}) => {
-  const { nodes } = params;
-  const node = nodes?.[0];
-  const value = encodedAttrs.styleId ?? node?.attributes?.['w:val'];
-
-  return {
-    type: 'attr',
-    xmlName: XML_NODE_NAME,
-    sdNodeOrKeyName: SD_ATTR_KEY,
-    attributes: { 'w:val': value ?? null },
-  };
-};
-
-/** @type {import('@translator').NodeTranslatorConfig} */
-export const config = {
-  xmlName: XML_NODE_NAME,
-  sdNodeOrKeyName: SD_ATTR_KEY,
-  type: NodeTranslator.translatorTypes.ATTRIBUTE,
-  encode,
-  attributes: validXmlAttributes,
-};
 
 /**
  * The NodeTranslator instance for the w:rStyle element.
  * @type {import('@translator').NodeTranslator}
+ * @see {@link https://ecma-international.org/publications-and-standards/standards/ecma-376/} "Fundamentals And Markup Language Reference", page 240
  */
-export const translator = NodeTranslator.from(config);
+export const translator = NodeTranslator.from(createSingleAttrPropertyHandler('w:rStyle', 'styleId'));
