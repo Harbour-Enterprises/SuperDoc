3c29cb403897731d2a7c8df429c91080081f896f
Artem Nistuley <101666502+artem-harbour@users.noreply.github.com>
2025-10-31 16:11:47 +0200

    fix: force track changes if input type is programmatic (#1199)

--- packages/super-editor/src/extensions/track-changes/trackChangesHelpers/trackedTransaction.js
+++ packages/super-editor/src/extensions/track-changes/trackChangesHelpers/trackedTransaction.js
@@ -17,10 +17,11 @@ import { CommentsPluginKey } from '../../comment/comments-plugin.js';
 export const trackedTransaction = ({ tr, state, user }) => {
   const onlyInputTypeMeta = ['inputType', 'uiEvent', 'paste', 'pointer'];
   const notAllowedMeta = ['historyUndo', 'historyRedo', 'acceptReject'];
+  const isProgrammaticInput = tr.getMeta('inputType') === 'programmatic';
 
   if (
     !tr.steps.length ||
-    (tr.meta && !Object.keys(tr.meta).every((meta) => onlyInputTypeMeta.includes(meta))) ||
+    (tr.meta && !Object.keys(tr.meta).every((meta) => onlyInputTypeMeta.includes(meta)) && !isProgrammaticInput) ||
     notAllowedMeta.includes(tr.getMeta('inputType')) ||
     tr.getMeta(CommentsPluginKey) // Skip if it's a comment transaction.
   ) {
