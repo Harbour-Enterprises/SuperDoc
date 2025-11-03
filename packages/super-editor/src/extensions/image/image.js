d3f1e4742af0982d616382b155056119029a84c3
Johannes Wilm <mail@johanneswilm.org>
2025-11-03 22:50:23 +0100

    fix: allow top/bottom images to float right and left (#1203)

--- packages/super-editor/src/extensions/image/image.js
+++ packages/super-editor/src/extensions/image/image.js
@@ -323,7 +323,7 @@ export const Image = Node.create({
           break;
 
         case 'Through':
-        case 'Tight':
+        case 'Tight': {
           style += 'clear: both;';
 
           const pageStylesData = getDataFromPageStyles({
@@ -377,12 +377,12 @@ export const Image = Node.create({
             style += `shape-outside: polygon(${points});`;
           }
           break;
-
+        }
         case 'TopAndBottom':
           style += 'display: block; clear: both;';
+
           if (attrs.distTop) margin.top += attrs.distTop;
           if (attrs.distBottom) margin.bottom += attrs.distBottom;
-          centered = true;
           break;
 
         case 'Inline':
@@ -409,6 +409,16 @@ export const Image = Node.create({
           }
           if (anchorData.alignH === 'left' || anchorData.alignH === 'right') {
             style += `position: absolute; ${anchorData.alignH}: 0;`;
+          }
+          break;
+        case 'column':
+          if (anchorData.alignH === 'center') {
+            centered = true;
+          } else if (anchorData.alignH === 'right') {
+            floatRight = true;
+            if (!style.includes('float: right;')) {
+              style += 'float: right;';
+            }
           }
           break;
         default:
