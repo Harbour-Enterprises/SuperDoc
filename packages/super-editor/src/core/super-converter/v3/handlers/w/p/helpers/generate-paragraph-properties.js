ce333629e446e9c73a2466be17d58ccd4ec17f2d
Luccas Correa <luccas@superdoc.dev>
2025-11-20 09:32:08 -0300

    feat: modify paragraph import/export to not override inline properties

--- packages/super-editor/src/core/super-converter/v3/handlers/w/p/helpers/generate-paragraph-properties.js
+++ packages/super-editor/src/core/super-converter/v3/handlers/w/p/helpers/generate-paragraph-properties.js
@@ -13,32 +13,6 @@ export function generateParagraphProperties(params) {
   const { attrs = {} } = node;
 
   const paragraphProperties = carbonCopy(attrs.paragraphProperties || {});
-  if (attrs.styleId !== paragraphProperties.styleId) {
-    paragraphProperties.styleId = attrs.styleId;
-  }
-
-  // Check which properties have changed
-  ['borders', 'styleId', 'indent', 'textAlign', 'keepLines', 'keepNext', 'spacing', 'tabStops'].forEach((key) => {
-    let propKey = key === 'textAlign' ? 'justification' : key;
-    if (JSON.stringify(paragraphProperties[propKey]) !== JSON.stringify(attrs[key])) {
-      paragraphProperties[propKey] = attrs[key];
-    }
-  });
-
-  const framePr = attrs.dropcap;
-  if (framePr) {
-    framePr.dropCap = framePr.type;
-    delete framePr.type;
-  }
-  if (JSON.stringify(paragraphProperties.framePr) !== JSON.stringify(framePr)) {
-    paragraphProperties.framePr = framePr;
-  }
-
-  // Get run properties from marksAttrs
-  const marksProps = decodeRPrFromMarks(attrs.marksAttrs || []);
-  const finalRunProps = combineRunProperties([paragraphProperties.runProperties || {}, marksProps]);
-  paragraphProperties.runProperties = finalRunProps;
-
   const pPr = wPPrNodeTranslator.decode({ node: { ...node, attrs: { paragraphProperties } } });
   const sectPr = node.attrs?.paragraphProperties?.sectPr;
   if (sectPr) {
