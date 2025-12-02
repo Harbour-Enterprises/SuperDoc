dd4cc5d925fe1b6560acef486777119e6e5c47c0
Gabriel Chittolina <gabriel@harbourshare.com>
2025-12-02 10:03:20 -0300

    chore: rename test

--- packages/super-editor/src/core/super-converter/v3/handlers/w/r/r-translator.test.js
+++ packages/super-editor/src/core/super-converter/v3/handlers/w/r/r-translator.test.js
@@ -124,7 +124,7 @@ describe('w:r r-translator (node)', () => {
     expect(result.content[2].type).toBe('text');
   });
 
-  it('returns a run node containing a comment range start and end', () => {
+  it('does not wrap a comment range start and end in a run node', () => {
     const params = {
       node: {
         type: 'run',
