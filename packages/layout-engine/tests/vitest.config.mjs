46a65f2a16e2143380ec91d71a9614b3f23a64c6
Tony Novak <tony.novak@harbourshare.com>
2025-12-15 14:48:37 -0500

    fix: build layout-engine/tests with "source" condition set
    Note that this requires adding back the super-editor aliases to allow
    references within the super-editor source to @core etc to be resolved.

--- packages/layout-engine/tests/vitest.config.mjs
+++ packages/layout-engine/tests/vitest.config.mjs
@@ -13,4 +13,16 @@ export default defineConfig({
       enabled: false,
     },
   },
+  resolve: {
+    conditions: ['source'],
+    alias: {
+      '@converter': resolve(__dirname, '../../super-editor/src/core/super-converter'),
+      '@core': resolve(__dirname, '../../super-editor/src/core'),
+      '@extensions': resolve(__dirname, '../../super-editor/src/extensions'),
+      '@components': resolve(__dirname, '../../super-editor/src/components'),
+      '@helpers': resolve(__dirname, '../../super-editor/src/core/helpers'),
+      '@tests': resolve(__dirname, '../../super-editor/src/tests'),
+      '@translator': resolve(__dirname, '../../super-editor/src/core/super-converter/v3/node-translator/index.js'),
+    },
+  },
 });
