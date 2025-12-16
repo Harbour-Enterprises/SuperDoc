352697ff4dd4fd906b7a837329e32a4c9b2d6bf1
Tony Novak <tony.novak@harbourshare.com>
2025-12-16 10:06:28 -0500

    feat: add shared config for vitest with "source" condition
    Include baseConfig in all vitest configuration to resolve using "source"
    condition and to set super-editor aliases

---
+++ vite.sourceResolve.ts
@@ -0,0 +1,23 @@
+import path from 'path';
+
+const sourceResolve = {
+  conditions: ['source'],
+  alias: [
+    { find: '@core', replacement: path.resolve(__dirname, 'packages/super-editor/src/core') },
+    { find: '@extensions', replacement: path.resolve(__dirname, 'packages/super-editor/src/extensions') },
+    { find: '@features', replacement: path.resolve(__dirname, 'packages/super-editor/src/features') },
+    { find: '@components', replacement: path.resolve(__dirname, 'packages/super-editor/src/components') },
+    { find: '@helpers', replacement: path.resolve(__dirname, 'packages/super-editor/src/core/helpers') },
+    { find: '@converter', replacement: path.resolve(__dirname, 'packages/super-editor/src/core/super-converter') },
+    { find: '@tests', replacement: path.resolve(__dirname, 'packages/super-editor/src/tests') },
+    {
+      find: '@translator',
+      replacement: path.resolve(
+        __dirname,
+        'packages/super-editor/src/core/super-converter/v3/node-translator/index.js',
+      ),
+    },
+  ],
+};
+
+export default sourceResolve;
